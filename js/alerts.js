db9edc9bc5314b2048dd54c4e7b8da0955809053
tatianaelizabethvalencia <tatianaelizabethvalencia@gmail.com>
2024-03-14 23:46:36 -0600

    sweet alerts agregados

---
+++ js/alerts.js
@@ -0,0 +1,19 @@
+function alert_generico(texto, tipo){
+    swal(texto, "", tipo);
+}
+
+function alert_exito_txt(texto){
+    swal(texto, "", "success");
+}
+
+function alert_exito(){
+    swal("Exito", "", "success");
+}
+
+function alert_error_txt(texto){
+    swal(texto, "", "error");
+}
+
+function alert_error(){
+    swal("Error", "", "error");
+}
